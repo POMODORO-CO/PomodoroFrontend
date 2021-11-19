66fbde85a01b4f92f7cc16632ea37c2c593cd2fc
Didier Garcia Cifuentes <didieralbertog@gmail.com>
2021-11-19 17:26:05 -0500

    Finalizo diseño de la interfaz de usuario de acuerdo a Mockup

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,69 +2,42 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-200">
-            <div className="container mx-auto">
-                <div className="text-center text-3xl leading-9 font-extrabold text-gray-800">
-                    ¡Bienvenid@s!
-                </div>
-                <div className="text-center text-teal-400 m-4"> Inicio de sesión</div>
-                <form >
-                    <div className="flex justify-center">
-                        <div className="lg:w-1/3 md:w-2/3 w-full">
-                            <label className="block uppercase tracking-wide text- text-xs font-bold mb-2"
-                                htmlFor="email" >
-                                Correo electrónico
-                            </label>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="test.js@gmail.com"
-                                className="bg-gray-300 border-gray-600 rounded w-full py-2 px-2 text-gray-700"
-                                required
-                            />
-                        </div>
+        <div className='min-h-screen grid place-content-center bg-gray-100'>
+            <div class="w-full max-w-xs m-6">
+                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <img class="w-20 h-20 rounded-full mx-auto m-5" src="https://images.pexels.com/photos/2589649/pexels-photo-2589649.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image" />
+                        <label class="block text-blue-900 text-sm font-bold py-1" for="username">
+                            Correo electrónico
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Correo electrónico" />
                     </div>
-
-                    <div className="flex justify-center mt-4">
-                        <div className="lg:w-1/3 md:w-2/3 w-full">
-                            <label
-                                className="block uppercase tracking-wide text-xs font-bold mb-2"
-                                htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="*********"
-                                className="bg-gray-300 border-gray-600 rounded w-full py-2 px-2 text-gray-700"
-                                required
-                            />
-                            <label class="inline-flex items-center text-ls py-2">
-                                <input type="checkbox" class="form-checkbox"/>
-                                <span class="ml-2">Recordar contraseña</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div className="mt-4 flex justify-center">
-                        <button class="
-                            lg:w-1/3 md:w-2/3 w-full
-                            py-2 
-                            px-20 
-                            font-semibold 
-                            rounded-lg 
-                            shadow-md 
-                            text-white 
-                            hover:text-blue-900 
-                            bg-blue-900 
-                            hover:bg-yellow-400">
+                    <div class="mb-6">
+                        <label class="block text-blue-900 text-sm font-bold mb-2" for="password">
+                            Contraseña
+                        </label>
+                        <input class="shadow appearance-none border border-blue-900 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="*******" />
+                        <p class="text-yellow-400 text-xs italic p-1">Por favor ingrese su contraseña.</p>
+                        <button class="bg-blue-900 hover:bg-yellow-400 text-white font-bold w-full py-2 px-3 rounded focus:outline-none focus:shadow-outline" type="button">
                             Ingresar
                         </button>
                     </div>
+                    <div>
+                        <label class="inline-flex items-center">
+                            <input type="checkbox" class="form-checkbox"/>
+                            <span class="ml-2">Recordar contraseña</span>
+                        </label>
+                    </div>
+                    <div class="flex items-center justify-between my-3">
+                        <a class="inline-block align-baseline font-bold text-sm text-blue-900 hover:text-blue-800" href="#">
+                            Olvidó su contraseña?
+                        </a>
+                    </div>
                 </form>
             </div>
-            </div>
+        </div>
     )
 }
+
 export default Login
+
