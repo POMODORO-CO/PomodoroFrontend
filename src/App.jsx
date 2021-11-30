1c9322ed07e21ec1d02af32ae51f94de44ce15a4
nticorag <nticoragps4@gmail.com>
2021-11-29 21:42:57 -0500

    Eliminacion de carpeta de proyectos redundante

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
-<<<<<<< HEAD
-import MiPerfil from './Pages/Usuarios/Miperfil/MiPerfil';
-import GestionUsuarios from './Pages/Usuarios/GestionUsuarios/GestionUsuarios';
-import Proyecto from './Pages/Proyectos/Proyecto';
-import Consulta from "./Pages/Proyectos/Consulta";
-import Inscripciones from "./Pages/Proyectos/Inscripciones";
-import Avances from "./Pages/Proyectos/Avances";
-
-=======
 import MiPerfil from './Pages/Users/Miperfil/MiPerfil';
 import GestionUsuarios from './Pages/Users/GestionUsuarios/GestionUsuarios';
 import Proyecto from './Pages/Projects/Project';
+import Consulta from "./Pages/Projects/Consulta";
+import Inscripciones from "./Pages/Projects/Inscripciones";
 import Login from './Pages/Login/Login';
->>>>>>> DidierGarcia
+import Avances from './Pages/Projects/Avances';
 
 
 function App() {
