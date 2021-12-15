900c9326a9a7eb8cefd377e3dc8a033a5ec16118
nticorag <nticoragps4@gmail.com>
2021-12-14 20:09:14 -0500

    Hu 19 y casi 20

--- src/Pages/Projects/ConsultasProyectos/Users/EstudianteConsulta.jsx
+++ src/Pages/Projects/ConsultasProyectos/Users/EstudianteConsulta.jsx
@@ -2,60 +2,81 @@ import React from 'react'
 import "../../../../index.css";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { GET_PROYECTOS } from '../../../../graphql/projects/queriesProjects';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import { INCRIPCION_ESTUDIANTE } from '../../../../graphql/projects/mutationsprojects';
 
 function EstudianteConsulta() {
 
-    const { data:dataProjects, error:errorProjects, loading:loadingProjects } = useQuery(GET_PROYECTOS);
+    const { data: dataProjects, error: errorProjects, loading: loadingProjects } = useQuery(GET_PROYECTOS);
+    const { data: dataInscripcion, error: errorInscripcion, loading: loadingInscripcion} = useMutation(INCRIPCION_ESTUDIANTE);
+
+    const submit = () => {
+        confirmAlert({
+          title: 'Inscripcion a proyecto',
+          message: '¿Confirmas tu inscripción a este proyecto?',
+          buttons: [
+            {
+              label: 'Sí',
+              onClick: () => alert('Inscripción exitosa')
+            },
+            {
+              label: 'No',
+              onClick: () => alert('No se realizó la inscripción')
+            }
+          ]
+        });
+      };
 
     return (
         <>
-            
-                <div className="flex items-center flex-col text-middle">
 
-                    <div className="box pt-6">
-                        <div className="box-wrapper">
+            <div className="flex items-center flex-col text-middle">
 
-                            <div className=" bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
-                                <button className="outline-none focus:outline-none"><svg className=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
-                                <input type="search" name="" id="" placeholder="Buscar.." x-model="q" className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent" />
-                                <div className="select">
-                                    <select name="" id="" x-model="image_type" className="text-sm outline-none focus:outline-none bg-transparent">
-                                        <option value="all" defaultValue>Todo</option>
-                                        <option value="photo">ID</option>
-                                        <option value="illustration">Nombre del proyecto</option>
-                                        <option value="vector">Líder</option>
-                                    </select>
-                                </div>
+                <div className="box pt-6">
+                    <div className="box-wrapper">
+
+                        <div className=" bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
+                            <button className="outline-none focus:outline-none"><svg className=" w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+                            <input type="search" name="" id="" placeholder="Buscar.." x-model="q" className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent" />
+                            <div className="select">
+                                <select name="" id="" x-model="image_type" className="text-sm outline-none focus:outline-none bg-transparent">
+                                    <option value="all" defaultValue>Todo</option>
+                                    <option value="photo">ID</option>
+                                    <option value="illustration">Nombre del proyecto</option>
+                                    <option value="vector">Líder</option>
+                                </select>
                             </div>
-
                         </div>
+
                     </div>
+                </div>
 
 
-                    <div className="-my-1 overflow-x-auto sm:-mx-6 lg:-mx-2 py-4">
-                        <div className="py-2 align-middle inline-block min-w-auto sm:px-6 lg:px-12">
-                            <div
-                                className="
+                <div className="-my-1 overflow-x-auto sm:-mx-6 lg:-mx-2 py-4">
+                    <div className="py-2 align-middle inline-block min-w-auto sm:px-6 lg:px-12">
+                        <div
+                            className="
 shadow
 overflow-hidden
 border-b border-gray-200
 sm:rounded-lg
 "
-                            >
-                                <table className="min-w-auto divide-y divide-gray-200">
-                                    <thead className="bg-gray-50">
-                                        <tr>
-                                            <th
-                                                scope="col"
-                                                className="hidden"
-                                            >
-                                                ID
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="
+                        >
+                            <table className="min-w-auto divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="hidden"
+                                        >
+                                            ID
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="
 px-6
 py-3
 text-left text-xs
@@ -64,12 +85,12 @@ text-gray-500
 uppercase
 tracking-wider
 "
-                                            >
-                                                Nombre del proyectos
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="
+                                        >
+                                            Nombre del proyectos
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="
 px-6
 py-3
 text-left text-xs
@@ -78,12 +99,40 @@ text-gray-500
 uppercase
 tracking-wider
 "
-                                            >
-                                                Líder
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                className="
+                                        >
+                                            Líder
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="
+px-6
+py-3
+text-left text-xs
+font-medium
+text-gray-500
+uppercase
+tracking-wider
+"
+                                        >
+                                            Estado
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="
+px-6
+py-3
+text-left text-xs
+font-medium
+text-gray-500
+uppercase
+tracking-wider
+"
+                                        >
+                                            Fase
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="
 px-6
 py-3
 text-center text-xs
@@ -92,21 +141,21 @@ text-gray-500
 uppercase
 tracking-wider
 "
-                                            >
-                                                Acciones
-                                            </th>
-                                            {/* <th scope="col" className="relative px-6 py-3">
+                                        >
+                                            Acciones
+                                        </th>
+                                        {/* <th scope="col" className="relative px-6 py-3">
 <span className="sr-only">Edit</span>
 </th> */}
-                                        </tr>
-                                    </thead>
-                                    <tbody className="bg-white divide-y divide-gray-200">
-                                        {dataProjects && dataProjects.Proyectos.map((u) => {
-                                            return (
-                                                <tr key={u._id}>
-                                                    <td className="hidden">
-                                                        <span
-                                                            className="
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {dataProjects && dataProjects.Proyectos.map((u) => {
+                                        return (
+                                            <tr key={u._id}>
+                                                <td className="hidden">
+                                                    <span
+                                                        className="
 px-2
 inline-flex
 text-xs
@@ -116,13 +165,13 @@ rounded-full
 bg-green-100
 text-green-800
 "
-                                                        >
-                                                            {u._id}
-                                                        </span>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap">
-                                                        <span
-                                                            className="
+                                                    >
+                                                        {u._id}
+                                                    </span>
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    <span
+                                                        className="
 px-2
 inline-flex
 text-xs
@@ -132,13 +181,13 @@ rounded-full
 bg-green-100
 text-green-800
 "
-                                                        >
-                                                            {u.nombre_proyecto}
-                                                        </span>
-                                                    </td>
-                                                    <td className="px-6 py-4 whitespace-nowrap">
-                                                        <span
-                                                            className="
+                                                    >
+                                                        {u.nombre_proyecto}
+                                                    </span>
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    <span
+                                                        className="
 px-2
 inline-flex
 text-xs
@@ -148,38 +197,74 @@ rounded-full
 bg-green-50
 text-green-800
 "
-                                                        >
-                                                            {u.lider_proyecto.nombre_usuario + " " + u.lider_proyecto.apellido_usuario}
-                                                        </span>
-                                                    </td>
-                                                    <td
+                                                    >
+                                                        {u.lider_proyecto.nombre_usuario + " " + u.lider_proyecto.apellido_usuario}
+                                                    </span>
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    <span
                                                         className="
+px-2
+inline-flex
+text-xs
+leading-5
+font-semibold
+rounded-full
+bg-green-50
+text-green-800
+"
+                                                    >
+                                                        {u.estado_proyecto}
+                                                    </span>
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap">
+                                                    <span
+                                                        className="
+px-2
+inline-flex
+text-xs
+leading-5
+font-semibold
+rounded-full
+bg-green-50
+text-green-800
+"
+                                                    >
+                                                        {u.fase_proyecto}
+                                                    </span>
+                                                </td>
+                                                <td
+                                                    className="
 px-6
 py-4
 whitespace-nowrap
 text-center text-sm
 font-medium
 "
-                                                    >
-                                                        <a href="#" className="inline-block"
-                                                        ><AiIcons.AiFillPlusCircle size={25} /></a>
-                                                        <a href="#" className="inline-block"
-                                                        ><FaIcons.FaEdit size={25} /></a>
-                                                        <a href="#" className="inline-block"
-                                                        ><FaIcons.FaTrash size={25} /></a>
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })}
+                                                >
+                                                    {u.estado_proyecto == "ACTIVO" ?
+                                                        <>
+                                                            <a href="#" className="inline-block"
+                                                            ><AiIcons.AiFillPlusCircle size={25} onClick={submit}  /></a>
+                                                            <a href="#" className="inline-block"
+                                                            ><FaIcons.FaEdit size={25} /></a>
+                                                            <a href="#" className="inline-block"
+                                                            ><FaIcons.FaTrash size={25} /></a>
+                                                        </>
+                                                        : null}
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
 
 
-                                    </tbody>
-                                </table>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-           
+            </div>
+
 
         </>
 
