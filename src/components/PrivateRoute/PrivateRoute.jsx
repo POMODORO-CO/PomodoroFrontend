ef1e76cffef4ec428d163e6fafbb0843a56640c8
Didier Garcia Cifuentes <didieralbertog@gmail.com>
2021-12-17 15:49:39 -0500

    Mensaje usuario pendiente

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useUser } from "../../context/userContext";
 import { Link } from "react-router-dom";
-import Login from '../Login/Login';
+
 
 const PrivateRoute = ({ rolelist, children }) => {
-const { userData } = useUser();
+    const { userData } = useUser();
 
     if (rolelist.includes(userData.rol_usuario)) {
         if (userData.estado_usuario !== "PENDIENTE") {
@@ -12,19 +12,17 @@ const { userData } = useUser();
         } else {
             return (
                 (<div className='min-h-screen flex justify-center items-center bg-gray-500'>
-                    <div className='bg-yellow-400 rounded-full flex min-w-max p-2 justify-center items-center'>
-                        <h1 className='py-2 px-1 text-2xl'>Usted no tiene un estado ACTIVO</h1>
-                        <h4 className='py-2 px-1 animate-pulse text-2xl font-bold'>{` - Estado actual: ${userData.estado_usuario}`}</h4>
-                        <div className="py-2 px-4">
-                        <Link to={'../../Pages/Login'}>
-                                <button className="py-2 px-4 text-white font-bold rounded-full bg-blue-900 shadow-lg block md:inline-block">Ir atrás</button></Link>
-                        </div>
-                    </div>
+                    <section className='bg-white inline-flex justify-center items-center rounded-full'>
+                        <section className='bg-white text-black font-semibold px-2 rounded-full m-1 '>
+                            Usted no tiene un estado ACTIVO
+                        </section>
+                        <p className='text-black px-3 py-2 bg-yellow-400 rounded-full m-1 font-bold'> {` - Estado actual: ${userData.estado_usuario}`}</p>
+                    </section>
+                    <Link to={'../../Pages/Landingpage/Landingpage.jsx'}>
+                        <button className="py-2 px-4 text-white font-bold rounded-full bg-blue-900 shadow-lg block md:inline-block">Ir atrás</button></Link>
                 </div>))
         }
-
     }
-
     return (
         <>
             <div data-testid="not-authorized">
