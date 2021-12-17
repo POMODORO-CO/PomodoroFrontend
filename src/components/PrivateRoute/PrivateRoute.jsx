bb1558729f3cbd91d4ae463b9ac17b4b703f198c
Didier Garcia Cifuentes <didieralbertog@gmail.com>
2021-12-17 15:37:46 -0500

    Creación mensaje estado actual

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { useUser } from "../../context/userContext";
 import { Link } from "react-router-dom";
+import Login from '../Login/Login';
+
 const PrivateRoute = ({ rolelist, children }) => {
-    const { userData } = useUser();
+const { userData } = useUser();
 
     if (rolelist.includes(userData.rol_usuario)) {
         if (userData.estado_usuario !== "PENDIENTE") {
             return children;
         } else {
             return (
-                <div>
-                    <h1>Usuario usted no tiene un estado ACTIVO</h1>
-                    <h4>{`su estado es ${userData.estado_usuario}`}</h4>
-                    <div className="py-2 px-6">
-                        <Link to={`/`}>
-                            <button className="py-2 px-6 text-white font-bold rounded-full bg-blue-900 shadow-lg block md:inline-block">Ir atrás</button></Link>
+                (<div className='min-h-screen flex justify-center items-center bg-gray-500'>
+                    <div className='bg-yellow-400 rounded-full flex min-w-max p-2 justify-center items-center'>
+                        <h1 className='py-2 px-1 text-2xl'>Usted no tiene un estado ACTIVO</h1>
+                        <h4 className='py-2 px-1 animate-pulse text-2xl font-bold'>{` - Estado actual: ${userData.estado_usuario}`}</h4>
+                        <div className="py-2 px-4">
+                        <Link to={'../../Pages/Login'}>
+                                <button className="py-2 px-4 text-white font-bold rounded-full bg-blue-900 shadow-lg block md:inline-block">Ir atrás</button></Link>
+                        </div>
                     </div>
-
-                </div>
-            )
+                </div>))
         }
 
     }
